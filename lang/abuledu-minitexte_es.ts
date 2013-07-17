3a8fa2a34f0054c4f1cccfe72f9441b4cd35476e
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-07-17 12:12:33 +0200

    Traduction en anglais

--- lang/abuledu-minitexte_es.ts
+++ lang/abuledu-minitexte_es.ts
@@ -2,23 +2,10 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="es_ES">
 <context>
-    <name>AbulEduTexteV1</name>
-    <message>
-        <location filename="../ui_abuledutextev1.h" line="52"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>MainWindow</name>
     <message utf8="true">
         <location filename="../mainwindow.ui" line="14"/>
         <source>Mini traitement de texte pour AbulÉdu</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="101"/>
-        <source>:/abuledutextev1/buttons/leaf</source>
         <translation type="unfinished"></translation>
     </message>
     <message utf8="true">
@@ -42,16 +29,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="231"/>
-        <source>:/abuledutextev1/buttons/new</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="234"/>
-        <source>:/abuledutextev1/buttons/newHover</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="262"/>
         <source>open</source>
         <translation type="unfinished"></translation>
@@ -59,16 +36,6 @@
     <message>
         <location filename="../mainwindow.ui" line="268"/>
         <source>&amp;Ouvrir</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="299"/>
-        <source>:/abuledutextev1/buttons/open</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="302"/>
-        <source>:/abuledutextev1/buttons/openHover</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -82,16 +49,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="367"/>
-        <source>:/abuledutextev1/buttons/save</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="370"/>
-        <source>:/abuledutextev1/buttons/saveHover</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="398"/>
         <source>quit</source>
         <translation type="unfinished"></translation>
@@ -99,16 +56,6 @@
     <message>
         <location filename="../mainwindow.ui" line="404"/>
         <source>&amp;Quitter</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="435"/>
-        <source>:/abuledutextev1/buttons/quit</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="438"/>
-        <source>:/abuledutextev1/buttons/quitHover</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -123,16 +70,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="500"/>
-        <source>:/abuledutextev1/buttons/help</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="503"/>
-        <source>:/abuledutextev1/buttons/helpHover</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="525"/>
         <source>print</source>
         <translation type="unfinished"></translation>
@@ -143,16 +80,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="559"/>
-        <source>:/abuledutextev1/buttons/print</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="562"/>
-        <source>:/abuledutextev1/buttons/printHover</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="584"/>
         <source>data</source>
         <translation type="unfinished"></translation>
@@ -160,46 +87,6 @@
     <message>
         <location filename="../mainwindow.ui" line="587"/>
         <source>&amp;Data</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="618"/>
-        <source>:/abuledutextev1/buttons/data</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="621"/>
-        <source>:/abuledutextev1/buttons/dataHover</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="696"/>
-        <source>:/abuledutextev1/buttons/play</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="699"/>
-        <source>:/abuledutextev1/buttons/play-gris</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="712"/>
-        <source>:/abuledutextev1/buttons/pause</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="715"/>
-        <source>:/abuledutextev1/buttons/pause-gris</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="728"/>
-        <source>:/abuledutextev1/buttons/stop</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="731"/>
-        <source>:/abuledutextev1/buttons/stop-gris</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
