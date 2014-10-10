78e00bf909f9c35d8624da0329a77c0763574f6f
Icham Sirat <icham.sirat@ryxeo.com>
2014-10-10 15:24:28 +0200

    [#3933] Retour pageTexte apres chargement module

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -88,6 +88,9 @@ MainWindow::MainWindow(QWidget *parent) :
     /* Le curseur a été déplacé*/
     connect(ui->teZoneTexte, SIGNAL(cursorPositionChanged()), this, SLOT(slotCursorMoved()), Qt::UniqueConnection);
 
+    /* #3933 Affichage texte au chargement d'un abe*/
+    connect(ui->abeBoxFileManager, SIGNAL(signalAbeFileSelected(QSharedPointer<AbulEduFileV1>)), this, SLOT(showTextPage()), Qt::UniqueConnection);
+
     initMultimedia();
     initSignalMapperFontChange();
     initSignalMapperFormFontChange();
@@ -140,30 +143,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->btn_leftText->click();  /* Alignement à gauche par défaut */
     m_textCharFormat = ui->teZoneTexte->textCursor().charFormat();
 
-//#ifdef __MICROTEXTE_MODE__
-//        ui->frmMenuFeuille->hide();
-//        ui->frmFormat->hide();
+    ui->frTopMicroTexte->hide();
+    ui->lblTitreMicroTexte->hide();
 
-//        /* Connexion des boutons */
-//        connect(ui->btnNewMicroTexte, SIGNAL(clicked()), SLOT(on_abeMenuFeuilleBtnNew_clicked()), Qt::UniqueConnection);
-//        connect(ui->btnOpenMicroTexte, SIGNAL(clicked()), SLOT(on_abeMenuFeuilleBtnOpen_clicked()), Qt::UniqueConnection);
-//        connect(ui->btnSaveMicroTexte, SIGNAL(clicked()), SLOT(on_abeMenuFeuilleBtnSave_clicked()), Qt::UniqueConnection);
-//        connect(ui->btnMajusculeMicroTexte, SIGNAL(clicked()), SLOT(slotFontCaps()), Qt::UniqueConnection);
-//        connect(ui->btnMinusculeMicroTexte, SIGNAL(clicked()), SLOT(slotFontLower()), Qt::UniqueConnection);
-//        // connect(ui->btnCursiveMicroTexte, SIGNAL(clicked()), SLOT(slotChangeFont(QString)));
-//        connect(ui->btnDecreasePoliceMicroTexte, SIGNAL(clicked()), SLOT(on_btn_decrease_clicked()), Qt::UniqueConnection);
-//        connect(ui->btnIncreasePoliceMicroTexte, SIGNAL(clicked()), SLOT(on_btn_increase_clicked()), Qt::UniqueConnection);
-//        connect(ui->btnPrintMicroTexte, SIGNAL(clicked()), SLOT(on_abeMenuFeuilleBtnPrint_clicked()), Qt::UniqueConnection);
-//        connect(ui->btnHelpMicroTexte, SIGNAL(clicked()), SLOT(on_abeMenuFeuilleBtnHelp_clicked()), Qt::UniqueConnection);
-
-//        connect(ui->btnQuitMicroTexte, SIGNAL(clicked()), abeApp, SLOT(quit()), Qt::UniqueConnection);
-
-//        /* Caps par defaut */
-//        ui->btnMajusculeMicroTexte->click();
-//#else
-        ui->frTopMicroTexte->hide();
-        ui->lblTitreMicroTexte->hide();
-//#endif
 }
 
 void MainWindow::centrerFenetre()
