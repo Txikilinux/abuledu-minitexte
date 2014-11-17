d4ad336b0b62fc02e010041a4b8b34bde8c46ffe
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2014-11-17 10:25:58 +0100

    Incrémentation version 1.0.0 -> 1.0.2

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,0,0
-#define VER_FILEVERSION_STR         "1.0.0\0"
+#define VER_FILEVERSION             1,0,2,0
+#define VER_FILEVERSION_STR         "1.0.2\0"
 
-#define VER_PRODUCTVERSION          1,0,0,0
-#define VER_PRODUCTVERSION_STR      "1.0.0\0"
+#define VER_PRODUCTVERSION          1,0,2,0
+#define VER_PRODUCTVERSION_STR      "1.0.2\0"
 
 #define VER_FILEDESCRIPTION_STR     "Tout petit editeur de texte"
 #define VER_INTERNALNAME_STR        "abuledu-minitexte"
