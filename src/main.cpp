9a645223d6ed7ccfedec5a7ad74c9b854893a6ef
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2015-03-20 11:50:38 +0100

    Démarrage en mode Maximized

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     w->showFullScreen();
   #else
     /* Splashscreen */
-    AbulEduSplashScreenV1 *splash = new AbulEduSplashScreenV1(0,true,Qt::WindowNoState);
+    AbulEduSplashScreenV1 *splash = new AbulEduSplashScreenV1(0,true,Qt::WindowMaximized);
     splash->abeSplashScreenLauchUtilitaire(true);
     splash->show();
     abeApp->processEvents();
