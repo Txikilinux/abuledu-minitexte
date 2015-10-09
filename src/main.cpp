f58f21eda9c441fc1ee98d62e541a07098d0794f
Icham Sirat <icham.sirat@ryxeo.com>
2015-10-09 14:28:31 +0200

    initLogger ou CommandLineParser gérés par les main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,11 @@ int main(int argc, char *argv[])
 {
     AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, VER_COMPANYNAME_STR, VER_UNITVERSION_STR);
     a.setAbeApplicationLongName(QObject::trUtf8(VER_PRODUCTNAME_STR));
+#if QT_VERSION >= 0x050200
+    a.initCommandLineParser();
+#else
+    a.initLogger();
+#endif
 
     MainWindow *w;
 
