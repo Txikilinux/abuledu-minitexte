6b8a4291cfa131766527b1d1a0dd5a69ae78c413
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2016-01-22 16:35:55 +0100

    Incrémentation du numéro de version en 1.0.4

--- src/version.h
+++ src/version.h
@@ -1,18 +1,18 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,2,0
-#define VER_FILEVERSION_STR         "1.0.2\0"
+#define VER_FILEVERSION             1,0,4,0
+#define VER_FILEVERSION_STR         "1.0.4\0"
 
-#define VER_PRODUCTVERSION          1,0,2,0
-#define VER_PRODUCTVERSION_STR      "1.0.2\0"
+#define VER_PRODUCTVERSION          1,0,4,0
+#define VER_PRODUCTVERSION_STR      "1.0.4\0"
 
 #define VER_FILEDESCRIPTION_STR     "Tout petit editeur de texte"
 #define VER_INTERNALNAME_STR        "abuledu-minitexte"
 #define VER_ORIGINALFILENAME_STR    "abuledu-minitexte.exe"
 #define VER_PRODUCTNAME_STR         "AbulEdu MiniTexte"
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012 RyXeo SARL"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2016 RyXeo SARL"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    "License GNU/GPL v3"
 #define VER_COMPANYDOMAIN_STR       "abuledu.org"
